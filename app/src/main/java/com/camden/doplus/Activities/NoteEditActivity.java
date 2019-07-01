47606ce4921270cbc90cfd38a182d97eaa728a14
camden <3293676794@qq.com>
2019-07-01 22:27:30 +0800

    完成了Note编辑界面的编写

--- app/src/main/java/com/camden/doplus/Activities/NoteEditActivity.java
+++ app/src/main/java/com/camden/doplus/Activities/NoteEditActivity.java
@@ -1,15 +1,81 @@
 package com.camden.doplus.Activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.camden.doplus.R;
 
+import jp.wasabeef.richeditor.RichEditor;
+
 public class NoteEditActivity extends AppCompatActivity {
+
+    private RichEditor mEditor;
+    private String mEditorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_edit);
+
+
+        mEditor = findViewById(R.id.wv_editor);
+        mEditor.setEditorHeight(200);
+        mEditor.setEditorFontSize(15);
+        mEditor.setEditorFontColor(Color.BLACK);
+        mEditor.setPadding(20, 20, 20, 10);
+        mEditor.setPlaceholder("从此处开始书写...");
+
+
+        mEditor.setOnTextChangeListener(new RichEditor.OnTextChangeListener() {
+            @Override public void onTextChange(String text) {
+                mEditorText = text;
+            }
+        });
+
+
+        findViewById(R.id.btn_headline_1).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(2);
+            }
+        });
+
+        findViewById(R.id.btn_headline_2).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setHeading(3);
+            }
+        });
+
+        findViewById(R.id.btn_bold).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setBold();
+            }
+        });
+
+        findViewById(R.id.btn_bullet_list).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setBullets();
+            }
+        });
+
+        findViewById(R.id.btn_order_list).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setNumbers();
+            }
+        });
+
+        findViewById(R.id.btn_indent).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setIndent();
+            }
+        });
+
+        findViewById(R.id.btn_no_indent).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                mEditor.setOutdent();
+            }
+        });
+
     }
 }
